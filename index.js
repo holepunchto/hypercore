93b6c83259ac624599a2e76701df9d072cdfe45f
Mathias Buus <mathiasbuus@gmail.com>
2016-01-22 19:09:01 +0100

    add storage api. fixes #3

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ function Hypercore (db, opts) {
   events.EventEmitter.call(this)
 
   this.db = db
+  this.cache = subleveldown(db, 'cache', {valueEncoding: 'binary'})
   this._hashes = subleveldown(db, 'hashes', {valueEncoding: 'binary'})
   this._blocks = subleveldown(db, 'blocks', {valueEncoding: 'binary'})
   this._bitfields = subleveldown(db, 'bitfields', {valueEncoding: 'binary'})
   this._feeds = subleveldown(db, 'feeds', {valueEncoding: messages.Feed})
+  this._storage = opts.storage || null
   this._opened = {}
 
   this.swarm = swarm(this, opts)
