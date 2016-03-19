cdd696669080fcce75e20d560c549683fd5661c5
Mathias Buus <mathiasbuus@gmail.com>
2016-03-19 16:25:14 -0700

    basic replication works over new protocol

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var crypto = require('crypto')
 var subleveldown = require('subleveldown')
 var bulk = require('bulk-write-stream')
 var collect = require('stream-collector')
@@ -8,8 +9,10 @@ var hash = require('./lib/hash')
 
 module.exports = Hypercore
 
-function Hypercore (db) {
-  if (!(this instanceof Hypercore)) return new Hypercore(db)
+function Hypercore (db, opts) {
+  if (!(this instanceof Hypercore)) return new Hypercore(db, opts)
+
+  this.id = opts && opts.id || crypto.randomBytes(32)
 
   this._db = db // TODO: needs levelup-defaults to force binary?
   this._nodes = subleveldown(db, 'nodes', {valueEncoding: messages.Node})
@@ -17,6 +20,8 @@ function Hypercore (db) {
   this._signatures = subleveldown(db, 'signatures', {valueEncoding: 'binary'})
   this._feeds = subleveldown(db, 'feeds', {valueEncoding: messages.Feed})
   this._bitfields = subleveldown(db, 'bitfields', {valueEncoding: 'binary'})
+
+  this._open = {}
 }
 
 Hypercore.publicId = Hypercore.prototype.publicId = function (key) {
@@ -27,9 +32,28 @@ Hypercore.prototype.createFeed = function (key, opts) {
   if (typeof key === 'string') key = Buffer(key, 'hex')
   if (key && !Buffer.isBuffer(key)) return this.createFeed(null, key)
   if (!opts) opts = {}
-  opts.key = key
+  if (!opts.key) opts.key = key
   opts.live = opts.live !== false // default to live feeds
+
   return feed(this, opts)
+
+  // strictly speaking we do not need to track fully downloaded
+  // feeds across the instance
+
+  // var keyHex = opts.key && opts.key.toString('hex')
+  // if (keyHex && this._open[keyHex]) return this._open[keyHex]
+
+  // var f = feed(this, opts)
+  // if (f.key) {
+  //   var self = this
+  //   keyHex = f.key.toString('hex')
+  //   this._open[keyHex] = f
+  //   f.on('close', function () { // TODO: move this into lib/feed?
+  //     if (self._open[keyHex] === f) delete self._open[keyHex]
+  //   })
+  // }
+
+  // return f
 }
 
 Hypercore.prototype.list = function (cb) {
@@ -57,8 +81,11 @@ Hypercore.prototype.createWriteStream = function (opts) {
   function flush (cb) {
     feed.finalize(function (err) {
       if (err) return cb(err)
-      patch(stream, feed)
-      cb()
+      feed.close(function (err) {
+        if (err) return cb(err)
+        patch(stream, feed)
+        cb()
+      })
     })
   }
 }
@@ -75,6 +102,7 @@ Hypercore.prototype.createReadStream = function (key, opts) {
 }
 
 function patch (stream, feed) {
+  stream.feed = feed
   stream.key = feed.key
   stream.publicId = feed.publicId
   stream.live = feed.live
