6866d0df6add58928c438c68eb6e4f299d6d9f44
Julian Gruber <julian@juliangruber.com>
2016-06-02 12:33:40 +0200

    add comment about test encoding

--- test/helpers/create.js
+++ test/helpers/create.js
@@ -2,5 +2,6 @@ var hypercore = require('../../')
 var memdb = require('memdb')
 
 module.exports = function create () {
+  // ensure hypercore overwrites the db's default encoding
   return hypercore(memdb({keyEncoding: 'json'}))
 }
