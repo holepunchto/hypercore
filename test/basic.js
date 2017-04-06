84186c7b15c874cf6eede4315eeb66093ede3561
Mathias Buus <mathiasbuus@gmail.com>
2017-04-06 06:30:12 +0200

    add explicit secret key test

--- test/basic.js
+++ test/basic.js
@@ -1,4 +1,5 @@
 var create = require('./helpers/create')
+var sodium = require('sodium-native')
 var tape = require('tape')
 
 tape('append', function (t) {
@@ -45,6 +46,21 @@ tape('flush', function (t) {
   feed.flush(function (err) {
     t.error(err, 'no error')
     t.same(feed.length, 1, '1 block')
+    t.end()
+  })
+})
+
+tape('pass in secret key', function (t) {
+  var secretKey = new Buffer(sodium.crypto_sign_SECRETKEYBYTES)
+  var key = new Buffer(sodium.crypto_sign_PUBLICKEYBYTES)
+  sodium.crypto_sign_keypair(key, secretKey)
+
+  var feed = create(key, {secretKey: secretKey})
+
+  feed.on('ready', function () {
+    t.same(feed.key, key)
+    t.same(feed.secretKey, secretKey)
+    t.ok(feed.writable)
     t.end()
   })
 })
