45887b8e67aabbec044d7ca05735af16b702ac13
Paul Frazee <pfrazee@gmail.com>
2017-11-06 20:33:10 -0600

    Add storageCacheSize option

--- test/basic.js
+++ test/basic.js
@@ -151,3 +151,39 @@ tape('head', function (t) {
     })
   }
 })
+
+tape('append, no cache', function (t) {
+  t.plan(8)
+
+  var feed = create({valueEncoding: 'json', storageCacheSize: 0})
+
+  feed.append({
+    hello: 'world'
+  })
+
+  feed.append([{
+    hello: 'verden'
+  }, {
+    hello: 'welt'
+  }])
+
+  feed.flush(function () {
+    t.same(feed.length, 3, '3 blocks')
+    t.same(feed.byteLength, 54, '54 bytes')
+
+    feed.get(0, function (err, value) {
+      t.error(err, 'no error')
+      t.same(value, {hello: 'world'})
+    })
+
+    feed.get(1, function (err, value) {
+      t.error(err, 'no error')
+      t.same(value, {hello: 'verden'})
+    })
+
+    feed.get(2, function (err, value) {
+      t.error(err, 'no error')
+      t.same(value, {hello: 'welt'})
+    })
+  })
+})
