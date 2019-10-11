1e0851da42ef1906e87536761d81d8fcf81387f6
Mathias Buus <mathiasbuus@gmail.com>
2019-10-11 14:15:01 +0200

    spec out new extension api (#231)
    * spec out new extension api
    
    * always set encoding
    
    * review by @andrewosh

--- index.js
+++ index.js
@@ -75,9 +75,7 @@ function Feed (createStorage, key, opts) {
   this.allowPush = !!opts.allowPush
   this.peers = []
   this.ifAvailable = new Nanoguard()
-
-  // Extensions must be sorted for handshaking to work
-  this.extensions = (opts.extensions || []).sort()
+  this.extensions = []
 
   this.crypto = opts.crypto || defaultCrypto
 
@@ -208,11 +206,80 @@ Feed.prototype.replicate = function (initiator, opts) {
   opts = opts || {}
   opts.stats = !!this._stats
 
-  if (!opts.extensions) opts.extensions = this.extensions
-
   var stream = replicate(this, initiator, opts)
   this.emit('replicating', stream)
   return stream
+}
+
+class Extension {
+  constructor (name, feed, handlers) {
+    this.name = name
+    this.id = 0
+    this.feed = feed
+    this.handlers = handlers || {}
+    this.encoding = codecs(this.handlers.encoding || 'binary')
+  }
+
+  remoteSupports (peer) {
+    return !!peer.remoteExtensions && peer.remoteExtensions.indexOf(this.name) > -1
+  }
+
+  onmessage (buffer, peer) {
+    if (!this.handlers.onmessage) return
+
+    const encoding = this.encoding
+    let message = null
+
+    try {
+      message = encoding.decode(buffer)
+    } catch (err) {
+      if (this.handlers.onerror) this.handlers.onerror(err)
+      return
+    }
+
+    this.handlers.onmessage(message, peer)
+  }
+
+  broadcast (message) {
+    const buf = this.encoding.encode(message)
+    for (const peer of this.feed.peers) {
+      peer.extension(this.id, buf)
+    }
+  }
+
+  send (message, peer) {
+    peer.extension(this.id, this.encoding.encode(message))
+  }
+
+  destroy () {
+    for (let i = 0; i < this.feed.extensions.length; i++) {
+      if (this.feed.extensions[i] === this) {
+        this.feed.extensions.splice(i, 1)
+        this.feed._updateExtensions()
+        return
+      }
+    }
+  }
+}
+
+Feed.prototype.registerExtension = function (name, handlers) {
+  const ext = new Extension(name, this, handlers)
+
+  this.extensions.push(ext)
+  this.extensions.sort(function (a, b) {
+    return a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+  })
+
+  this._updateExtensions()
+
+  return ext
+}
+
+Feed.prototype._updateExtensions = function () {
+  for (let i = 0; i < this.extensions.length; i++) {
+    this.extensions[i].id = i
+  }
+  for (const peer of this.peers) peer._updateOptions()
 }
 
 Feed.prototype.setDownloading = function (downloading) {
