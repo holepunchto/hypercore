f9fb513344a633f0ae3c8a545b0ab8dea41b3301
Mathias Buus <mathiasbuus@gmail.com>
2019-10-14 14:38:22 +0200

    fix small bug when using setUploading/setDownloading

--- test/set-uploading-downloading.js
+++ test/set-uploading-downloading.js
@@ -62,3 +62,30 @@ tape('setUploading', function (t) {
     })
   })
 })
+
+tape('get block while not uploading', function (t) {
+  const feed = create()
+
+  feed.append(['a', 'b', 'c', 'd'], function () {
+    const clone = create(feed.key, { sparse: true })
+
+    clone.get(0, function (err, data) {
+      t.error(err, 'no error')
+      t.same(data, Buffer.from('a'))
+      feed.setUploading(false)
+      clone.get(3, function (err, data) {
+        t.ok(feed.uploading)
+        t.ok(clone.peers[0].remoteUploading)
+        t.error(err, 'no error')
+        t.same(data, Buffer.from('d'))
+        t.end()
+      })
+
+      setImmediate(function () {
+        feed.setUploading(true)
+      })
+    })
+
+    replicate(feed, clone)
+  })
+})
