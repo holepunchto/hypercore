d1aaf41ee148f3a7ee9852ecc2a60ba8c7f46804
Mauve <ranger@mauve.moe>
2020-04-09 15:47:18 -0400

    Rename .destroy to .destroyData

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ var replicate = require('./lib/replicate')
 var Protocol = require('hypercore-protocol')
 var Message = require('abstract-extension')
 var Nanoresource = require('nanoresource/emitter')
+var util = require('util')
 
 class Extension extends Message {
   broadcast (message) {
@@ -1354,7 +1355,7 @@ Feed.prototype.flush = function (cb) {
   this.append([], cb)
 }
 
-Feed.prototype.destroy = function (cb) {
+Feed.prototype.destroyData = function (cb) {
   const self = this
 
   this.close(function (err) {
@@ -1362,6 +1363,10 @@ Feed.prototype.destroy = function (cb) {
     else self._storage.destroy(cb)
   })
 }
+
+Feed.prototype.destroy = util.deprecate(function (cb) {
+  this.destroyData(cb)
+}, 'feed.destroy() is deprecated. Use feed.destroyData() instead.')
 
 Feed.prototype._close = function (cb) {
   const self = this
