0cee228e3cfe642cfe11b4a6a7c494ecd84d79ac
Mathias Buus <mathiasbuus@gmail.com>
2021-02-04 21:02:39 +0100

    remove dead test

--- test/basic.js
+++ test/basic.js
@@ -21,26 +21,3 @@ tape('basic', async function (t) {
 
   t.end()
 })
-
-tape('basic clone', async function (t) {
-  const core = await create()
-  const clone = await create(core.key)
-
-  await core.append('hello')
-  await core.append('a')
-  await core.append('world')
-
-  const p = await core.proof({ block: { index: 1, nodes: 0, value: true }, upgrade: { start: 0, length: 3 } })
-
-  await clone.verify(p)
-
-  t.same(clone.length, 3)
-  t.same(clone.byteLength, 11)
-  t.same(await clone.get(1), Buffer.from('a'))
-
-  t.same(await clone.has(0), false)
-  t.same(await clone.has(1), true)
-  t.same(await clone.has(2), false)
-
-  t.end()
-})
