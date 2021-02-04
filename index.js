755880a699657393b0d9524412bf7151a0523f6c
Mathias Buus <mathiasbuus@gmail.com>
2021-02-04 10:32:16 +0100

    fix writable check during load

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ module.exports = class Omega extends EventEmitter {
   }
 
   get writable () {
-    return this.info.secretKey !== null
+    return this.info !== null && this.info.secretKey !== null
   }
 
   get length () {
