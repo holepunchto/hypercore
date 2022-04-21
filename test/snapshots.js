6ff5e69ec77ab10d1c1bc18f67c3a541f17ad716
Mathias Buus <mathiasbuus@gmail.com>
2022-04-21 11:34:07 +0200

    add initial snapshot consistency tests

---
+++ test/snapshots.js
@@ -0,0 +1,71 @@
+const { create } = require('./helpers')
+const test = require('brittle')
+
+test('implicit snapshot - gets are snapshotted at call time', async function (t) {
+  t.plan(8)
+
+  const core = await create()
+  const clone = await create(core.key, { valueEncoding: 'utf-8' })
+
+  clone.on('truncate', function (len) {
+    t.is(len, 2, 'remote truncation')
+  })
+
+  core.on('truncate', function (len) {
+    t.is(len, 2, 'local truncation')
+  })
+
+  await core.append('block #0.0')
+  await core.append('block #1.0')
+  await core.append('block #2.0')
+
+  const r1 = replicate(core, clone)
+
+  t.is(await clone.get(0), 'block #0.0')
+
+  await unreplicate(r1)
+
+  const range1 = core.download({ start: 0, end: 4 })
+  const range2 = clone.download({ start: 0, end: 4 })
+
+  const p2 = clone.get(1)
+  const p3 = clone.get(2)
+
+  await core.truncate(2)
+
+  await core.append('block #2.1')
+  await core.append('block #3.1')
+
+  replicate(core, clone)
+
+  t.is(await p2, 'block #1.0')
+  t.exception(p3, 'should fail cause snapshot not available')
+
+  t.is(await clone.get(2), 'block #2.1')
+
+  await range1.downloaded()
+  t.pass('local range finished')
+
+  await range2.downloaded()
+  t.pass('remote range finished')
+})
+
+function replicate (a, b) {
+  const s1 = a.replicate(true)
+  const s2 = b.replicate(false)
+
+  s1.pipe(s2).pipe(s1)
+
+  return [s1, s2]
+}
+
+function unreplicate (streams) {
+  const ps = streams.map(s => {
+    return new Promise((resolve) => {
+      s.on('error', () => {})
+      s.on('close', resolve)
+      s.destroy()
+    })
+  })
+  return Promise.all(ps)
+}
