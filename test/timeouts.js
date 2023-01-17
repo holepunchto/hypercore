15869239def4eb61570f4b501480d7b7357e69c0
Lucas Barrena <lucasbarrena98@gmail.com>
2023-01-17 13:47:52 -0300

    add tests for remote cores

--- test/timeouts.js
+++ test/timeouts.js
@@ -1,5 +1,5 @@
 const test = require('brittle')
-const { create } = require('./helpers')
+const { create, replicate } = require('./helpers')
 const b4a = require('b4a')
 
 test('get before timeout', async function (t) {
@@ -44,6 +44,87 @@ test('get after 0ms timeout', async function (t) {
   }).catch((err) => {
     t.is(err.code, 'REQUEST_TIMEOUT')
   })
+})
+
+test('timeout but hits cache (remote await)', async function (t) {
+  t.plan(2)
+
+  const a = await create({ cache: true })
+  const b = await create(a.key, { cache: true })
+  replicate(a, b, t)
+
+  try {
+    const block = await b.get(0, { timeout: 0 })
+    t.fail('should not have got block: ' + block)
+  } catch (err) {
+    t.is(err.code, 'REQUEST_TIMEOUT', 'first request failed')
+  }
+
+  setTimeout(() => {
+    a.append('hi')
+  }, 100)
+
+  t.alike(await b.get(0), b4a.from('hi'), 'second request succeed')
+})
+
+test('timeout but hits cache (remote parallel)', async function (t) {
+  t.plan(2)
+
+  const a = await create({ cache: true })
+  const b = await create(a.key, { cache: true })
+  replicate(a, b, t)
+
+  const b1 = b.get(0, { timeout: 0 })
+  const b2 = b.get(0)
+
+  try {
+    const block = await b1
+    t.fail('should not have got block: ' + block)
+  } catch (err) {
+    t.is(err.code, 'REQUEST_TIMEOUT', 'first request failed')
+  }
+
+  setTimeout(() => {
+    a.append('hi')
+  }, 100)
+
+  t.alike(await b2, b4a.from('hi'), 'second request succeed')
+})
+
+test('timeout but hits cache (await)', async function (t) {
+  t.plan(2)
+
+  const core = await create({ cache: true })
+
+  try {
+    await core.get(0, { timeout: 0 })
+  } catch (error) {
+    t.is(error.code, 'REQUEST_TIMEOUT')
+  }
+
+  await core.append('hi')
+  t.alike(await core.get(0), b4a.from('hi'))
+})
+
+test('timeout but hits cache (parallel)', async function (t) {
+  t.plan(2)
+
+  const core = await create({ cache: true })
+
+  const b1 = core.get(0, { timeout: 0 })
+  const b2 = core.get(0)
+
+  try {
+    await b1
+  } catch (err) {
+    t.is(err.code, 'REQUEST_TIMEOUT')
+  }
+
+  setTimeout(() => {
+    core.append('hi')
+  }, 100)
+
+  t.alike(await b2, b4a.from('hi'))
 })
 
 test('get after timeout with await', async function (t) {
