567229e5e68f5caa04cce96fd9685e022c4c9553
Lucas Barrena <lucasbarrena98@gmail.com>
2023-06-21 15:24:15 -0300

    Cleanup

--- test/networking.js
+++ test/networking.js
@@ -3,9 +3,6 @@ const test = require('brittle')
 const speedometer = require('speedometer')
 const byteSize = require('tiny-byte-size')
 const UDX = require('udx-native')
-const DHT = require('hyperdht')
-const createTestnet = require('hyperdht/testnet')
-const Hyperswarm = require('hyperswarm')
 const Id = require('hypercore-id-encoding')
 const NoiseStream = require('@hyperswarm/secret-stream')
 const { create } = require('./helpers')
@@ -44,61 +41,6 @@ test.solo('replication speed', async function (t) {
     console.log('Blocks', '↓ ' + Math.ceil(info.blocks.down()), '↑ ' + Math.ceil(info.blocks.up()), 'Network', '↓ ' + byteSize(info.network.down()), '↑ ' + byteSize(info.network.up()), 'max', b.replicator.peers[0].maxInflight)
   }
 })
-
-/* test.skip('replication speed', { timeout: 99999999 }, async function (t) {
-  t.plan(1)
-
-  const opts = null // { latency: [250, 300] }
-  const dht = new DHT({ debug: { stream: opts } })
-  const swarm = new Hyperswarm({ dht })
-  t.teardown(() => swarm.destroy())
-  process.once('SIGINT', () => t.pass())
-  // return
-
-  const b = await create(Id.decode('4pr4zr8nyd1k7e8ntysnu6yrrwpmqyczs3obnx8dbnse3kf4jtzo'))
-
-  const info = track(b)
-
-  const done = b.findingPeers()
-  swarm.on('connection', (socket) => b.replicate(socket))
-  swarm.join(b.discoveryKey, { server: false, client: true })
-  swarm.flush().then(done, done)
-
-  let started = Date.now()
-  let cache = ''
-
-  b.on('download', onchange)
-  b.on('upload', onchange)
-  b.download()
-
-  function onchange () {
-    if (Date.now() - started < 250) {
-      return
-    }
-
-    started = Date.now()
-
-    console.log('Blocks', '↓ ' + Math.ceil(info.blocks.down()), '↑ ' + Math.ceil(info.blocks.up()), 'Network', '↓ ' + byteSize(info.network.down()), '↑ ' + byteSize(info.network.up()))
-
-    const id = 's' + Math.ceil(info.blocks.down.max || 0) + Math.ceil(info.blocks.up.max || 0) + byteSize(info.network.down.max || 0) + byteSize(info.network.up.max || 0)
-    if (cache === id) return
-    cache = id
-
-    return
-
-    // TODO: auto-detect when it repeats the same values 5 times to finish
-    console.log(
-      '[max]',
-      // b.contiguousLength,
-      'blks',
-      '↓ ' + Math.ceil(info.blocks.down.max || 0),
-      '↑ ' + Math.ceil(info.blocks.up.max || 0),
-      'net',
-      '↓ ' + byteSize(info.network.down.max || 0),
-      '↑ ' + byteSize(info.network.up.max || 0)
-    )
-  }
-}) */
 
 async function makeStreamPair (t, opts = {}) {
   const u = new UDX()
