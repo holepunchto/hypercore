6c939981a706f094d10601d86a5ec774f14e2adb
Lucas Barrena <lucasbarrena98@gmail.com>
2023-06-21 15:43:01 -0300

    Feedback

---
+++ test/helpers/networking.js
@@ -0,0 +1,65 @@
+const UDX = require('udx-native')
+const safetyCatch = require('safety-catch')
+const NoiseStream = require('@hyperswarm/secret-stream')
+
+module.exports = {
+  makeStreamPair
+}
+
+function makeStreamPair (t, opts = {}) {
+  const u = new UDX()
+  const a = u.createSocket()
+  const b = u.createSocket()
+
+  t.teardown(() => a.close())
+  t.teardown(() => b.close())
+
+  a.bind(0)
+  b.bind(0)
+
+  const p = proxy({ from: a, to: b }, async function () {
+    const delay = opts.latency[0] + Math.round(Math.random() * (opts.latency[1] - opts.latency[0]))
+    if (delay) await new Promise((resolve) => setTimeout(resolve, delay))
+    return false
+  })
+
+  t.teardown(() => p.close())
+
+  const s1 = u.createStream(1)
+  const s2 = u.createStream(2)
+
+  s1.connect(a, 2, p.address().port)
+  s2.connect(b, 1, p.address().port)
+
+  t.teardown(() => s1.destroy())
+  t.teardown(() => s2.destroy())
+
+  const n1 = new NoiseStream(true, s1)
+  const n2 = new NoiseStream(false, s2)
+
+  return [n1, n2]
+}
+
+function proxy ({ from, to, bind } = {}, handler) {
+  from = from.address().port
+  to = to.address().port
+
+  const u = new UDX()
+  const socket = u.createSocket()
+
+  socket.on('message', function (buf, rinfo) {
+    const forwarding = handler()
+    const port = rinfo.port === to ? from : to
+
+    if (forwarding && forwarding.then) forwarding.then(fwd).catch(safetyCatch)
+    else fwd(forwarding)
+
+    function fwd () {
+      socket.trySend(buf, port, '127.0.0.1')
+    }
+  })
+
+  socket.bind(bind || 0)
+
+  return socket
+}
--- test/networking.js
+++ test/networking.js
@@ -1,12 +1,8 @@
 const test = require('brittle')
-// const DebuggingStream = require('debugging-stream')
 const speedometer = require('speedometer')
 const byteSize = require('tiny-byte-size')
-const UDX = require('udx-native')
-const Id = require('hypercore-id-encoding')
-const NoiseStream = require('@hyperswarm/secret-stream')
 const { create } = require('./helpers')
-const proxy = require('./helpers/udx-proxy.js')
+const { makeStreamPair } = require('./helpers/networking.js')
 
 test.solo('replication speed', async function (t) {
   const a = await create()
@@ -17,7 +13,7 @@ test.solo('replication speed', async function (t) {
 
   await a.append(new Array(15000).fill().map(() => Math.random().toString(16).substr(2)))
 
-  const [n1, n2] = await makeStreamPair(t, { latency: [25, 25] }) // Note: stream.rtt will be around twice this value
+  const [n1, n2] = makeStreamPair(t, { latency: [25, 25] }) // Note: stream.rtt will be around twice this value
   a.replicate(n1)
   b.replicate(n2)
 
@@ -33,53 +29,14 @@ test.solo('replication speed', async function (t) {
   await a.close()
 
   function onchange () {
-    // if (b.replicator.peers.length !== 1) throw new Error('Different number of peers')
+    if (b.replicator.peers.length !== 1) throw new Error('Different number of peers')
 
-    if (Date.now() - started < 250) return
+    if (Date.now() - started < 500) return
     started = Date.now()
 
-    console.log('Blocks', '↓ ' + Math.ceil(info.blocks.down()), '↑ ' + Math.ceil(info.blocks.up()), 'Network', '↓ ' + byteSize(info.network.down()), '↑ ' + byteSize(info.network.up()), 'max', b.replicator.peers[0].maxInflight)
+    t.comment('Blocks', '↓ ' + Math.ceil(info.blocks.down()), 'Network', '↓ ' + byteSize(info.network.down()), 'Max inflight', b.replicator.peers[0].maxInflight)
   }
 })
-
-async function makeStreamPair (t, opts = {}) {
-  const u = new UDX()
-
-  const a = u.createSocket()
-  const b = u.createSocket()
-
-  t.teardown(() => a.close())
-  t.teardown(() => b.close())
-
-  a.bind(0)
-  b.bind(0)
-
-  const p = await proxy({ from: a, to: b }, async function (pkt) {
-    const delay = opts.latency[0] + Math.round(Math.random() * (opts.latency[1] - opts.latency[0]))
-    if (delay) await sleep(delay)
-    return false
-  })
-
-  t.teardown(() => p.close())
-
-  const s1 = u.createStream(1)
-  const s2 = u.createStream(2)
-
-  s1.connect(a, 2, p.address().port)
-  s2.connect(b, 1, p.address().port)
-
-  t.teardown(() => s1.destroy())
-  t.teardown(() => s2.destroy())
-
-  const n1 = new NoiseStream(true, s1)
-  const n2 = new NoiseStream(false, s2)
-
-  return [n1, n2]
-}
-
-function sleep (ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
 
 // It could be built in Hypercore, at least a simpler version
 function track (core) {
@@ -103,4 +60,8 @@ function onspeed (eventName, info, index, byteLength, from) {
 
   if (block.max === undefined || blocks > block.max) block.max = blocks
   if (network.max === undefined || networks > network.max) network.max = networks
+}
+
+function sleep (ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
