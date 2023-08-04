e40fc5931a3f85d627c7d1646580f3a089e05434
Lucas Barrena <lucasbarrena98@gmail.com>
2023-08-04 18:44:21 -0300

    Add bench: speedtest replication with many peers (@mafintosh's bench) (#411)
    * Add bench: speedtest replication with many peers (@mafintosh's bench)
    
    * Tiny improvements around logging and timing
    
    * Wait for both downloads to be done

---
+++ test/bench/speedtest.js
@@ -0,0 +1,44 @@
+const test = require('brittle')
+const Hypercore = require('../../index.js')
+const RAM = require('random-access-memory')
+const { replicate } = require('../helpers')
+
+test('speedtest replication with many peers', { timeout: 120000 }, async function (t) {
+  const core = new Hypercore(RAM)
+  await core.ready()
+
+  const clone1 = new Hypercore(RAM, core.key)
+  const clone2 = new Hypercore(RAM, core.key)
+  const clone3 = new Hypercore(RAM, core.key)
+
+  for (let i = 0; i < 100000; i++) {
+    await core.append('#' + i)
+    if (i % 10000 === 0) t.comment('Append ' + i)
+  }
+
+  t.comment('Writer complete')
+
+  replicate(core, clone1, t)
+  replicate(core, clone2, t)
+  replicate(core, clone3, t)
+  replicate(clone1, clone2, t)
+  replicate(clone1, clone3, t)
+
+  const started = Date.now()
+  let count = 0
+
+  clone1.on('download', ondownload)
+  clone2.on('download', ondownload)
+
+  function ondownload () {
+    if (++count % 10000 === 0) t.comment('Downloaded ' + count + ' blocks after ' + (Date.now() - started) + ' ms')
+  }
+
+  const d1 = clone1.download({ start: 0, end: core.length })
+  const d2 = clone2.download({ start: 0, end: core.length })
+
+  await d1.done()
+  await d2.done()
+
+  t.comment('Done in ' + (Date.now() - started) + ' ms')
+})
