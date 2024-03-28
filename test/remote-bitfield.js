b5cb1aa25fa85cf9dd194675e9df1331c3f2ba98
Mathias Buus <mathiasbuus@gmail.com>
2024-03-28 14:59:11 +0100

    fix findFirst bitfield edge at the end of pages (#500)

--- test/remote-bitfield.js
+++ test/remote-bitfield.js
@@ -16,3 +16,14 @@ test('remote bitfield - set range on page boundary', function (t) {
 
   t.is(b.findFirst(true, 2048), 2048)
 })
+
+test('set last bits in segment and findFirst', function (t) {
+  const b = new RemoteBitfield()
+
+  b.set(32766, true)
+  t.is(b.findFirst(false, 32766), 32767)
+
+  b.set(32767, true)
+  t.is(b.findFirst(false, 32766), 32768)
+  t.is(b.findFirst(false, 32767), 32768)
+})
