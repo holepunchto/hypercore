10de034baf85611c800987518a6052fc448c4a38
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-04-26 16:08:22 +0100

    test we can recover from bad copyFrom

--- test/core.js
+++ test/core.js
@@ -677,6 +677,35 @@ test('core - copyFrom can recover from bad additional', async function (t) {
   t.alike(await clone.blocks.get(2), b4a.from('c'))
 })
 
+test('core - copyFrom can recover from bad additional', async function (t) {
+  const { core } = await create()
+  const { core: copy } = await create({ keyPair: core.header.keyPair })
+  const { core: clone } = await create({ keyPair: { publicKey: core.header.keyPair.publicKey } })
+
+  await core.append([b4a.from('a'), b4a.from('b')])
+  await copy.copyFrom(core, core.tree.signature)
+
+  t.is(copy.header.keyPair.publicKey, core.header.keyPair.publicKey)
+  t.is(copy.header.keyPair.publicKey, clone.header.keyPair.publicKey)
+
+  // copy should be independent
+  await core.append([b4a.from('c')])
+
+  await t.exception(clone.copyFrom(copy, core.tree.signature, { length: 3, additional: [b4a.from('d')] }))
+  await t.execution(clone.copyFrom(copy, core.tree.signature, { length: 3, additional: [b4a.from('c')] }))
+
+  t.is(clone.header.tree.length, 3)
+  t.alike(clone.header.tree.signature, core.header.tree.signature)
+
+  t.is(clone.tree.length, core.tree.length)
+  t.is(clone.tree.byteLength, core.tree.byteLength)
+  t.alike(clone.roots, core.roots)
+
+  t.alike(await clone.blocks.get(0), b4a.from('a'))
+  t.alike(await clone.blocks.get(1), b4a.from('b'))
+  t.alike(await clone.blocks.get(2), b4a.from('c'))
+})
+
 async function create (opts) {
   const storage = new Map()
 
