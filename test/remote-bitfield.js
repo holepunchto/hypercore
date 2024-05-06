1983ecf61e9e32d51f8fbdf103fcb02b2abb95fb
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-05-06 23:07:34 +0200

    (do not merge) illustrate remote-bitfield bug with contiguous length

--- test/remote-bitfield.js
+++ test/remote-bitfield.js
@@ -1,6 +1,43 @@
 const test = require('brittle')
 const RemoteBitfield = require('../lib/remote-bitfield')
 
+const b4a = require('b4a')
+const { create, replicate } = require('./helpers')
+
+test.solo('remote congituous length consistency', async function (t) {
+  const a = await create()
+  const b = await create(a.key)
+  const c = await create(a.key)
+
+  replicate(a, b, t)
+  replicate(b, c, t)
+
+  await a.append('block0')
+  await a.append('block1')
+
+  await new Promise(resolve => setTimeout(resolve, 500))
+
+  await b.get(0)
+  await new Promise(resolve => setTimeout(resolve, 500))
+
+  const peer = getPeer(c, b)
+  console.log('peer.remoteContiguousLength', peer.remoteContiguousLength, 'peer._remoteContiguousLength:', peer._remoteContiguousLength)
+  t.is(peer._remoteContiguousLength <= peer.remoteContiguousLength, true, 'check invar: remoteContiguousLength at least _remoteContiguousLength')
+})
+
+// Peer b as seen by peer a (b is the remote peer)
+function getPeer (a, b) {
+  for (const aPeer of a.replicator.peers) {
+    for (const bPeer of b.replicator.peers) {
+      if (b4a.equals(aPeer.stream.remotePublicKey, bPeer.stream.publicKey)) {
+        return aPeer
+      }
+    }
+  }
+
+  throw new Error('Error in test: peer not found')
+}
+
 test('remote bitfield - findFirst', function (t) {
   const b = new RemoteBitfield()
 
