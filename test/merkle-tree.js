ac6fcc6063ccbf96d19da869636581f6e727150e
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-06-20 11:26:43 +0100

    update to use peakLastTreeNode

--- test/merkle-tree.js
+++ test/merkle-tree.js
@@ -1,6 +1,7 @@
 const test = require('brittle')
 const b4a = require('b4a')
 const createTempDir = require('test-tmp')
+const CoreStorage = require('hypercore-on-the-rocks')
 const Tree = require('../lib/merkle-tree')
 
 test('nodes', async function (t) {
