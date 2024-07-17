65a06190012b0759c929591d497edbc859f2f6bb
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-07-17 09:49:46 +0100

    fix session get and truncate

--- index.js
+++ index.js
@@ -818,9 +818,9 @@ module.exports = class Hypercore extends EventEmitter {
     if (this.opened === false) await this.opening
     if (!isValidIndex(start) || !isValidIndex(end)) throw ASSERTION('has range is invalid')
 
-    if (end === start + 1) return this.core.bitfield.get(start)
+    if (end === start + 1) return this.state.bitfield.get(start)
 
-    const i = this.core.bitfield.firstUnset(start)
+    const i = this.state.bitfield.firstUnset(start)
     return i === -1 || i >= end
   }
 
@@ -978,18 +978,18 @@ module.exports = class Hypercore extends EventEmitter {
     if (this.opened === false) await this.opening
 
     const {
-      fork = this.core.tree.fork + 1,
+      fork = this.state.tree.fork + 1,
       keyPair = this.keyPair,
       signature = null
     } = typeof opts === 'number' ? { fork: opts } : opts
 
     const writable = !this._readonly && !!(signature || (keyPair && keyPair.secretKey))
-    if (writable === false && (newLength > 0 || fork !== this.core.tree.fork)) throw SESSION_NOT_WRITABLE()
+    if (writable === false && (newLength > 0 || fork !== this.state.tree.fork)) throw SESSION_NOT_WRITABLE()
 
     await this.core.truncate(this.state, newLength, fork, { keyPair, signature })
 
     // TODO: Should propagate from an event triggered by the oplog
-    this.replicator.updateAll()
+    if (this.state === this.core.state) this.replicator.updateAll()
   }
 
   async append (blocks, opts = {}) {
