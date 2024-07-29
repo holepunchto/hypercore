cf71ceeca7b4eff4d54a622bb98645f0c5efe883
Mathias Buus <mathiasbuus@gmail.com>
2024-07-29 14:28:24 +0200

    Tweak unslabbing (#544)
    * all batch sigs are unslabbed always and unslab root hash
    
    * unslab header buffers also
    
    * fix bad assertion

--- test/preload.js
+++ test/preload.js
@@ -30,7 +30,7 @@ test('preload - from another core', async function (t) {
   })
   await second.ready()
 
-  t.is(first.key, second.key)
+  t.alike(first.key, second.key)
   t.is(first.sessions, second.sessions)
 })
 
@@ -44,7 +44,7 @@ test('preload - custom keypair', async function (t) {
   await core.ready()
 
   t.ok(core.writable)
-  t.is(core.key, keyPair.publicKey)
+  t.alike(core.key, keyPair.publicKey)
 })
 
 test('preload - sign/storage', async function (t) {
