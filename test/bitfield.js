ec2e3594b0f92f7e654341d68fd9ff24adf4d374
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2024-10-16 17:19:34 +0200

    rename to hypercore storage (#582)

--- test/bitfield.js
+++ test/bitfield.js
@@ -1,7 +1,7 @@
 const test = require('brittle')
 const b4a = require('b4a')
 const createTempDir = require('test-tmp')
-const CoreStorage = require('hypercore-on-the-rocks')
+const CoreStorage = require('hypercore-storage')
 const Bitfield = require('../lib/bitfield')
 const BitInterlude = require('../lib/bit-interlude')
 
