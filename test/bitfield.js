a608949a799e34c18dbd57da96e16de9b4ca415b
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-02-04 15:03:55 +0100

    Failing tests for large-bitfield inconsistency

--- test/bitfield.js
+++ test/bitfield.js
@@ -272,6 +272,34 @@ test('set last bits in segment and findFirst', async function (t) {
   t.is(b.findFirst(false, 2097151), 2097152)
 })
 
+test.solo('bitfield - bitfields false by default', async function (t) {
+  const storage = await createStorage(t)
+  const b = await Bitfield.open(storage, 0)
+
+  let nrTrue = 0
+  for (let i = 0; i < 2 ** 20; i++) {
+    if (b.get(i) === true) nrTrue++
+  }
+
+  t.is(nrTrue, 0)
+})
+
+test.solo('bitfield - bitfields false by default if pushed after 2 ** 15', async function (t) {
+  const storage = await createStorage(t)
+  const b = await Bitfield.open(storage, 0)
+
+  for (let i = 0; i < 10 + 2 ** 15; i++) {
+    b.set(i, true)
+  }
+
+  let nrFalse = 0
+  for (let i = 0; i < 100 + 2 ** 15; i++) {
+    if (b.get(i) === false) nrFalse++
+  }
+
+  t.is(nrFalse, 90)
+})
+
 async function createStorage (t, dir) {
   if (!dir) dir = await createTempDir(t)
 
