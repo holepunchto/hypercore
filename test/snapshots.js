58feff7f4026f45ab10857cf34dd37f007ad45af
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-02-26 10:03:53 +0100

    Clarify assertion

--- test/snapshots.js
+++ test/snapshots.js
@@ -36,7 +36,7 @@ test('snapshot does not change when original gets modified', async function (t) 
   await core.append('new Block2')
   t.is(snap.length, 3, 'correct length')
   t.is(snap.signedLength, 2, 'signed length remains at lowest value after appending again to the original')
-  t.is(b4a.toString(await snap.get(2)), 'block2', 'block exists')
+  t.is(b4a.toString(await snap.get(2)), 'block2', 'Old block still (snapshot did not change)')
 
   {
     const res = []
