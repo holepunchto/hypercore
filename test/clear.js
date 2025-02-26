23d5848dd7b2362711667f57f73352c9fec95124
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-02-26 21:03:21 +0100

    Add clear test for large core (#643)
    * Add clear test for large core
    
    * Do not assert corestore size
    
    * No more need for timeouts
    
    * Add assertions at storage level
    
    * Clean up test

--- test/clear.js
+++ test/clear.js
@@ -1,6 +1,7 @@
 const test = require('brittle')
 const tmp = require('test-tmp')
 const b4a = require('b4a')
+const CoreStorage = require('hypercore-storage')
 const { create, createStorage, replicate, eventFlush } = require('./helpers')
 
 const Hypercore = require('../')
@@ -127,3 +128,60 @@ test('clear - no side effect from clearing unknown nodes', async function (t) {
 
   t.pass('did not crash')
 })
+
+test('clear - large cores', async function (t) {
+  t.timeout(100000)
+  const dir = await t.tmp()
+
+  const db = new CoreStorage(dir)
+  const a = new Hypercore(db)
+  await a.ready()
+  t.teardown(() => a.close(), { order: 1 })
+
+  const blocks = []
+  for (let i = 0; i < 300_000; i++) blocks.push(`Block-${i}`)
+  await a.append(blocks)
+
+  t.is(a.contiguousLength, 300_000, 'sanity check')
+  {
+    const storageBlocks = await consumeStream(a.state.storage.createBlockStream())
+    t.is(storageBlocks.length, 300_000, 'storage-level sanity check')
+  }
+
+  await a.clear(100, 1000)
+  await a.clear(2 ** 16 - 10, 2 ** 16 + 10) // 2 ** 16 is when the bitfield first changes pages, so interesting are to test
+  await a.clear(290000, 299998)
+
+  t.is(b4a.toString(await a.get(99)), 'Block-99')
+  t.is(await a.get(100, { wait: false }), null)
+  t.is(await a.get(999, { wait: false }), null)
+  t.is(b4a.toString(await a.get(1000)), 'Block-1000')
+  {
+    const storageBlocks = await consumeStream(a.state.storage.createBlockStream({ gte: 99, lte: 1000 }))
+    t.alike(storageBlocks.map(b => b.index), [99, 1000], 'correct state in hypercore storage')
+  }
+
+  t.is(b4a.toString(await a.get(2 ** 16 - 11)), 'Block-65525')
+  t.is(await a.get(2 ** 16 - 10, { wait: false }), null)
+  t.is(await a.get(2 ** 16 + 9, { wait: false }), null)
+  t.is(b4a.toString(await a.get(2 ** 16 + 10)), 'Block-65546')
+  {
+    const storageBlocks = await consumeStream(a.state.storage.createBlockStream({ gte: 2 ** 16 - 11, lte: 2 ** 16 + 10 }))
+    t.alike(storageBlocks.map(b => b.index), [65525, 65546], 'correct state in hypercore storage')
+  }
+
+  t.is(b4a.toString(await a.get(290000 - 1)), 'Block-289999')
+  t.is(await a.get(290000, { wait: false }), null)
+  t.is(await a.get(299997, { wait: false }), null)
+  t.is(b4a.toString(await a.get(299998)), 'Block-299998')
+  {
+    const storageBlocks = await consumeStream(a.state.storage.createBlockStream({ gte: 289999, lte: 299998 }))
+    t.alike(storageBlocks.map(b => b.index), [289999, 299998], 'correct state in hypercore storage')
+  }
+})
+
+async function consumeStream (rx) {
+  const res = []
+  for await (const b of rx) res.push(b)
+  return res
+}
