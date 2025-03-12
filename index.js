ed30c67dc6e2c1a444b8f73f1a15c535138e4715
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-03-12 12:15:32 +0100

    Do not emit bool with close event

--- index.js
+++ index.js
@@ -290,7 +290,7 @@ class Hypercore extends EventEmitter {
       if (this.state !== null) this.state.removeSession(this)
 
       this.closed = true
-      this.emit('close', this.core.hasSession() === false)
+      this.emit('close')
       throw err
     }
 
@@ -476,14 +476,14 @@ class Hypercore extends EventEmitter {
     if (this.core.hasSession()) {
       // emit "fake" close as this is a session
       this.closed = true
-      this.emit('close', false)
+      this.emit('close')
       return
     }
 
     if (this.core.autoClose) await this.core.close()
 
     this.closed = true
-    this.emit('close', true)
+    this.emit('close')
   }
 
   async commit (session, opts) {
