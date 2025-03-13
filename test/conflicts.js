c429123942f403dceb7e526d8507f29f41d8b155
Mathias Buus <mathiasbuus@gmail.com>
2025-03-13 16:48:45 +0100

    fix false positive in conflict detection

--- test/conflicts.js
+++ test/conflicts.js
@@ -1,7 +1,7 @@
 const test = require('brittle')
 const { create, replicate, unreplicate } = require('./helpers')
 
-test.skip('one forks', async function (t) {
+test('one forks', async function (t) {
   // NOTE: skipped because this test occasionally (~1/100) flakes
   // because one of the 'conflict' events never emits
   // due to a lifecycle issue (when closing all sessions
@@ -11,10 +11,14 @@ test.skip('one forks', async function (t) {
   const a = await create(t)
   await a.append(['a', 'b', 'c', 'd', 'e'])
 
+  a.core.name = 'a'
+
   const b = await create(t, a.key)
+  b.core.name = 'b'
 
   const c = await create(t, { keyPair: a.core.header.keyPair })
   await c.append(['a', 'b', 'c', 'd', 'f', 'e'])
+  c.core.name = 'c'
 
   const streams = replicate(a, b, t)
 
