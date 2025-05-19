6f1f3894379aeb09f5eb34e875cb6ff6384867bf
Mathias Buus <mathiasbuus@gmail.com>
2025-05-19 20:27:22 +0200

    add fully-remote-proof helpers

---
+++ test/fully-remote-proof.js
@@ -0,0 +1,24 @@
+const test = require('brittle')
+const b4a = require('b4a')
+const remote = require('../lib/fully-remote-proof.js')
+
+const { create } = require('./helpers')
+
+test('fully remote proof - proof and verify', async function (t) {
+  const core = await create(t)
+
+  await core.append('hello')
+  await core.append('world')
+
+  {
+    const proof = await remote.proof(core)
+    t.ok(await remote.verify(core.state.storage.store, proof))
+  }
+
+  {
+    const proof = await remote.proof(core, { index: 0, block: b4a.from('hello') })
+    const p = await remote.verify(core.state.storage.store, proof)
+    t.is(p.block.index, 0)
+    t.alike(p.block.value, b4a.from('hello'))
+  }
+})
