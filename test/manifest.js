8c42dad389dfb830639a4f4448e4e929603f8cf6
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2025-08-29 12:16:09 +0100

    Always ensure manifest is persisted (#725)
    * add test for manifest persistence
    
    * always persist manifest on open if provided
    
    * remove unused _manifestFlushed prop

--- test/manifest.js
+++ test/manifest.js
@@ -1487,6 +1487,54 @@ test('create verifier - open existing core with manifest', async function (t) {
   await compatCore.close()
 })
 
+test('manifest - persist if manifest is updated', async function (t) {
+  const keyPair = crypto.keyPair()
+
+  const manifest = Verifier.createManifest({
+    quorum: 1,
+    signers: [{
+      signature: 'ed25519',
+      publicKey: keyPair.publicKey
+    }]
+  })
+
+  const key = Verifier.manifestHash(manifest)
+  const create = await createStored(t)
+
+  {
+    const core = await create(key, { compat: false })
+    await core.ready()
+
+    t.is(core.manifest, null)
+    t.is(core.core.header.manifest, null)
+    t.alike(core.key, key)
+
+    await core.close()
+  }
+
+  {
+    const core = await create(key, { manifest, compat: false })
+    await core.ready()
+
+    t.not(core.manifest, null)
+    t.not(core.core.header.manifest, null)
+    t.alike(core.key, key)
+
+    await core.close()
+  }
+
+  {
+    const core = await create(key, { compat: false })
+    await core.ready()
+
+    t.not(core.manifest, null)
+    t.not(core.core.header.manifest, null)
+    t.alike(core.key, key)
+
+    await core.close()
+  }
+})
+
 function createMultiManifest (signers, prologue = null) {
   return {
     hash: 'blake2b',
