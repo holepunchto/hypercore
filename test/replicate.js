f7b056ad034e8fe9fbecef796bf627ba624af0a7
Mathias Buus <mathiasbuus@gmail.com>
2025-12-10 21:38:52 +0100

    prettier

--- test/replicate.js
+++ test/replicate.js
@@ -2590,7 +2590,7 @@ test('push mode', async function (t) {
 
   await a.append(['world1', 'world2'], {
     // post append is guaranteed to run post append but PRE
-    async postappend () {
+    async postappend() {
       await a.replicator.push(1)
       await a.replicator.push(2)
     }
