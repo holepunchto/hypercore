106cb0d4f8aa9457c85eff5b8ce41b23b83047a6
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2026-02-19 18:44:51 +0000

    Bail push if index is greater than length (#772)
    * add failing test
    
    * do not push an index greater than length
    
    * test only checks push execution
    
    * unsolo and include test in all.js
    
    * Recreate scenario w/ block flushed but length not updated in test
    
    We have no hooks for when flushing to storage (nor do we necessarily
    need one) and because flush is an IO op, creating a stable timing is
    nigh impossible. So the last resort was monkey patching to force the
    replication push to happen after flushing to storage.
    
    ---------
    
    Co-authored-by: Sean Zellmer <sean@lejeunerenard.com>

---
+++ test/push.js
@@ -0,0 +1,95 @@
+const test = require('brittle')
+const { create, replicate } = require('./helpers')
+
+test('no requests in pushOnly mode', async function (t) {
+  const a = await create(t)
+  const b = await create(t, a.key, { allowPush: true, pushOnly: true })
+
+  b.replicator.setPushOnly(true)
+
+  t.is(b.replicator.pushOnly, true)
+
+  replicate(a, b, t)
+
+  await a.append('1')
+  await a.append('2')
+  await a.append('3')
+
+  await t.exception(b.get(0, { timeout: 500 }), /REQUEST_TIMEOUT/)
+  await t.exception(b.get(1, { timeout: 500 }), /REQUEST_TIMEOUT/)
+  await t.exception(b.get(2, { timeout: 500 }), /REQUEST_TIMEOUT/)
+
+  t.ok(a.peers[0].remoteAllowPush)
+
+  await a.replicator.push(0)
+  await a.replicator.push(1)
+  await a.replicator.push(2)
+
+  await t.execution(b.get(0, { timeout: 500 }))
+  await t.execution(b.get(1, { timeout: 500 }))
+  await t.execution(b.get(2, { timeout: 500 }))
+})
+
+test('push and pull concurrently', async function (t) {
+  const a = await create(t)
+  const b = await create(t, a.key, { allowPush: true, pushOnly: true })
+
+  t.is(b.replicator.pushOnly, true)
+
+  replicate(a, b, t)
+
+  await new Promise((resolve) => a.on('peer-add', resolve))
+  for (let i = 0; i < 20; i++) {
+    await a.append(i.toString())
+  }
+
+  const bHasLength = new Promise((resolve) =>
+    b.on('append', () => {
+      if (b.length === 30) resolve()
+    })
+  )
+  const appends = []
+  for (let i = 20; i < 30; i++) {
+    appends.push(a.append(i.toString()).then(() => a.replicator.push(i)))
+  }
+
+  await b.get(10, { force: true })
+
+  await Promise.all(appends)
+  await bHasLength
+
+  t.pass('b synced length')
+  t.ok(await b.has(29))
+})
+
+test('push before append', async function (t) {
+  t.plan(2)
+  const a = await create(t)
+  const b = await create(t, a.key, { allowPush: true })
+
+  replicate(a, b, t)
+
+  await new Promise((resolve) => a.on('peer-add', resolve))
+
+  // wait for peer to sync
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const recv = new Promise((resolve) => b.on('append', resolve))
+
+  // To recreate the scenario where a block is flushed to storage but the length
+  // hasn't been updated yet, this monkey patch allows executing code directly
+  // after the SessionState flushes.
+  const originalFlush = a.core.state.flush.bind(a.core.state)
+  a.core.state.flush = async () => {
+    const result = await originalFlush()
+    await t.execution(a.replicator.push(0))
+    a.core.state.flush = originalFlush
+    return result
+  }
+
+  const send = a.append('hello world')
+
+  await Promise.all([send, recv])
+
+  t.ok(b.length, 'b synced length')
+})
