f3b2b0e8e806189ec1d19b0a06d238be23458be6
Sean Zellmer <sean@lejeunerenard.com>
2026-02-24 11:45:41 -0600

    Fix non-reverse mark stream & get being first op that loads from storage
    Added basic tests for marks bitfield.

--- test/mark-bitfield.js
+++ test/mark-bitfield.js
@@ -12,31 +12,53 @@ test('MarkBitfield - basic', async (t) => {
 })
 
 test('MarkBitfield - createMarkStream()', async (t) => {
-  t.plan(4)
-
   const core = await createCore(t)
   const bitfield = new MarkBitfield(core)
 
-  const expected = [10, 50000] // 50k for different page
+  const expected = [0, 32767, 32768, 50_000, 70_000]
 
   for (const i of expected) {
     await bitfield.set(i, true)
   }
 
-  const stream = bitfield.createMarkStream()
-  let i = 0
-  stream.on('data', (index) => {
-    t.is(expected[i++], index, 'got data')
-  })
-
-  await once(stream, 'end')
-
+  t.alike(await toArray(bitfield.createMarkStream()), expected)
   const reverseStream = bitfield.createMarkStream({ reverse: true })
-  reverseStream.on('data', (index) => {
-    t.is(expected[--i], index, 'got data : reverse')
+  t.alike(await toArray(reverseStream), expected.reverse())
+})
+
+test('MarkBitfield - load from storage', async (t) => {
+  const s = await createStorage(t)
+  const storage = await s.createCore({
+    key: Buffer.alloc(32),
+    discoveryKey: Buffer.alloc(32)
   })
 
-  await once(reverseStream, 'end')
+  {
+    const marks = new MarkBitfield(storage)
+
+    const expected = [0, MarkBitfield.BITS_PER_PAGE - 1, MarkBitfield.BITS_PER_PAGE, MarkBitfield.BITS_PER_PAGE * 2]
+    for (const i of expected) {
+      await marks.set(i, true)
+    }
+
+    const results = await toArray(marks.createMarkStream())
+    t.alike(results, expected, 'got stream of block indexes')
+  }
+
+  // Reloaded from storage
+  {
+    const marks = new MarkBitfield(storage)
+    t.ok(await marks.get(0), '1st page loaded from storage')
+    t.ok(await marks.get(MarkBitfield.BITS_PER_PAGE), '2nd page loaded from storage')
+
+    await t.execution(marks.set(MarkBitfield.BITS_PER_PAGE * 2 + 1, true))
+    t.ok(await marks.get(MarkBitfield.BITS_PER_PAGE * 2 + 1, true), '3rd page set')
+
+    await marks.clear()
+
+    const clearResults = await toArray(marks.createMarkStream())
+    t.alike(clearResults, [], 'clear removed all bits')
+  }
 })
 
 async function createCore(t) {
@@ -45,4 +67,14 @@ async function createCore(t) {
     key: Buffer.alloc(32),
     discoveryKey: Buffer.alloc(32)
   })
+}
+
+async function toArray(stream) {
+  const results = []
+
+  for await (const chunk of stream) {
+    results.push(chunk)
+  }
+
+  return results
 }
