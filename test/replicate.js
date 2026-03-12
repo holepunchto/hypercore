a190b076de41a018739d89b55c4a8638547e6918
hdegroote <75906619+HDegroote@users.noreply.github.com>
2026-03-12 23:00:03 +0100

    Bugfix: use backup logic for NOT_AVAILABLE replies

--- test/replicate.js
+++ test/replicate.js
@@ -2910,6 +2910,29 @@ test('local recovering from remote', async function (t) {
   t.is(b.length, a.length)
 })
 
+test('backoff respected for NOT_AVAILABLE in case of incorrectly false remote bitfield)', async function (t) {
+  const writer = await create(t)
+
+  await writer.append(['a', 'b', 'c', 'd', 'e'])
+
+  const badBitfielder = await create(t, writer.key)
+  const reader = await create(t, writer.key)
+
+  replicate(badBitfielder, writer, t)
+  await badBitfielder.get(0)
+
+  t.is(badBitfielder.core.bitfield.get(0), true, 'sanity check')
+  await badBitfielder.core.bitfield.set(0, false) // put incorrect bitfield
+
+  replicate(badBitfielder, reader, t)
+
+  await t.exception(async () => reader.get(0, { timeout: 500 }), /REQUEST_TIMEOUT/, 'cannot get the block due to bad bitfield')
+
+  t.is(reader.replicator.stats.notAvailableBackoffs, 32, 'paused after 32 attempts')
+  t.ok(badBitfielder.replicator.stats.wireRequest.rx < 40, 'did not continue getting spammed (sanity check)')
+  t.ok(reader.replicator.stats.wireRequest.tx < 40, 'did not continue spamming (sanity check)')
+})
+
 async function createAndDownload(t, core) {
   const b = await create(t, core.key)
   replicate(core, b, t, { teardown: false })
