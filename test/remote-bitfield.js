b04f890d6387ee00a318b60712fc218ccfc3d19f
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-04-27 15:56:02 +0800

    Run format

--- test/remote-bitfield.js
+++ test/remote-bitfield.js
@@ -1,5 +1,5 @@
 const test = require('brittle')
 test('simple test', async function (t) {
-  await new Promise(resolve => setTimeout(resolve, 1000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   t.pass('simple test passed')
 })
