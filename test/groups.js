3583b4713c83d335d8c73d9331ded5fe3bf9b4bf
Christophe Diederichs <chm-diederichs@protonmail.com>
2026-05-22 00:08:44 +0100

    fix group conflict tests

--- test/groups.js
+++ test/groups.js
@@ -51,22 +51,8 @@ test('groups - conflict', async function (t) {
 
   await a.close()
 
-  const b = new Hypercore(dir, { group: b4a.alloc(32, 2) })
-  await t.exception(b.ready())
-
-  await b.close()
-})
-
-test('groups - conflict', async function (t) {
-  const dir = await t.tmp()
-  const a = new Hypercore(dir)
-  await a.ready()
-
-  await a.setGroup(b4a.alloc(32, 1))
-  t.alike(a.core.header.group?.key, b4a.alloc(32, 1))
-
-  await a.close()
-
   const b = new Hypercore(dir)
   await t.exception(b.setGroup(b4a.alloc(32, 2)))
+
+  await b.close()
 })
